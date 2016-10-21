b44b8f09516ac8b5bec6a3da0503dfc718586722
jkcooley <cooley.jk8@gmail.com>
2016-10-21 14:22:55 -0500

    set up publishers, subscribers, callback functions, etc. for behavior 1

--- behavior_1.cpp
+++ behavior_1.cpp
@@ -1,20 +1,104 @@
+//tutorials used: https://github.com/utexas-bwi/segbot_arm/tree/master/segbot_arm_tutorials/src
+
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <sensor_msgs/joint_state.h>
+#include <sensor_msgs/jointState.h>
+#include <actionlib/client/simple_action_client.h>
+#include <jaco_msgs/FingerPosition.h>
+#include <jaco_msgs/HomeArm.h>
+#include <segbot_arm_manipulation/arm_utils.h>
 
+#define JOINTS 8
 //up-down motion
+
+//describes the state of a set of torque-controlled joints - name, position, velocity, effort (http://docs.ros.org/kinetic/api/sensor_msgs/html/msg/JointState.html)
+sensor_msgs::JointState joint_state;
+bool heard_state;
+
+//pose with reference coordinate frame and timestamp (http://docs.ros.org/api/geometry_msgs/html/msg/PoseStamped.html)
+geometry_msgs::PoseStamped pose_stamped;
+bool heard_pose_stamped;
+
+//http://docs.ros.org/hydro/api/jaco_msgs/html/msg/FingerPosition.html - note that this robot does not have a finger 3
+jaco_msgs::FingerPosition finger_pose;
+bool heard_finger_pose;
 
 //global publisher for cartesian velocity
 ros::Publisher velocity_pub;
 
-//finger 1
-//finger 2
-//finger 3 DNE, but could store anyway? 
+//callback function for joint state
+void joint_state_callback(const sensor_msgs::JointStateConstPtr& message)
+{
+	//if data from all the joints has been recieved, set joint_state and heard_state accordingly
+	if (message->position.size() == JOINTS)
+	{
+		joint_state = *message;
+		heard_state = true;
+	}
+}
 
-//joints to take note of... 
+//PoseStampedConstPtr& message or just PoseStamped &message
+//callback function for stamped position
+void pose_stamped_callback(const sensor_msgs::PoseStampedConstPtr& message)
+{
+	pose_stamped = *message;
+        heard_pose_stamped = true;
+}
+
+//callback function for finger position
+void finger_pose_callback(const sensor_msgs::FingerPositionConstPtr& message)
+{
+	finger_pose = *message;
+        heard_finger_pose = true;
+}
+
+//get the states from the arm
+void get_data()
+{
+	heard_state = false;
+	heard_pose_stamped = false;
+	heard_finger_pose = false;
+	
+	//40 Hz
+	ros::Rate r(40.0);
+	
+	while (ros::ok())
+	{
+		ros::spinOnce();	
+		
+		//if the data has been acquired, exit; else, loop back and try again while able
+		if (heard_state && heard_pose_stamped && heard_finger_pose)
+			return;
+		
+		r.sleep();
+	}
+}
+
+//call functions to get data
+int main(int argc, char **argv)
+{
+	//name the node
+	ros::init(argc, argv, "behavior_1");
+
+	ros::nodeHandle node_handle;
+
+	//subscribe to topics
+	ros::Subscriber joint_pose_subscriber = node_handle.subscribe("/joint_states", 1, joint_state_callback);
+	ros::Subscriber pose_stamped_subscriber = node_handle.subscribe("/mico_arm_driver/out/tool_position", 1, pose_stamped_callback);	
+	ros::Subscriber finger_pose_subscriber = node_handle.subscribe("/mico_arm_driver/out/finger_position", 1, finger_pose_callback);
+
+	//publish the velocities
+	ros::Publisher velocity_publisher = node_handle.advertise<geometry_msgs::TwistStamped>("/mico_arm_driver/in/cartesian_velocity", 10);
+
+	//get the states from the arm
+	get_data();
+
+
+	//this is the part where it goes to the position we want it in
+}
 
 //out/cartesian_velocity //subscriber, may be used to set velocity
 //out/joint_velocity //subscriber, may be used to set velocity
@@ -22,6 +106,7 @@ ros::Publisher velocity_pub;
 //out/joint_angles //raw angular position in degrees
 //out/joint_state //topic, publishes transformed joint angles in radians via sensor_msgs
 
+//what the joint state prints out
 /*
 string[] name
 float64[] position
